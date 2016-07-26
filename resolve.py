c84509a0374241d80da81be4afa45cf3ea4f6920
alwabel <alwabel@usc.edu>
2016-07-25 17:43:49 -0700

    finalize resolver

--- resolve.py
+++ resolve.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from dns import resolver
 from dns import reversename
+import dns
 import sys
 import threading
 
@@ -13,6 +14,7 @@ class reader_wrapper(object):
             return self.source.next()
     def __iter__(self):
         return self
+
 class writer_wrapper(object):
     def __init__(self,dest=sys.stdout):
         self.lock = threading.Lock()
@@ -20,26 +22,33 @@ class writer_wrapper(object):
     def write(self,msg):
         with self.lock:
             print >> self.dest, msg
+
 def resolve(ip):
     addr = reversename.from_address(ip)
-    result = resolver.query(addr,"PTR")
-    names = []
-    for r in result:
-        names.append( str(r) ) 
+    try:
+        result = resolver.query(addr,"PTR")
+        names = []
+        for r in result:
+            names.append( str(r)[:-1] ) 
+    except dns.resolver.NXDOMAIN:
+        return ["NXDOMAIN"]
+    except dns.resolver.NoNameservers:
+        return ["NoNameservers"]
+    except dns.resolver.Timeout:
+        return ["Timeout"]
+    except dns.resolver.NoAnswer:
+        return ["NoAnswer"]
+    except Exception as e:
+        print >> sys.stderr,"Exception {0}={1}={2}={3}".format(ip,str(e),type(e),e)
+        return [str(e)]
     return names
 
 def process(r,w):
     for ip in r:
         ip = ip.strip()
-        w.write("{0} ---> {1}".format(ip,resolve(ip)))
+        w.write("{0}\t{1}".format(ip,','.join(resolve(ip))))
         #print "{0} ---> {1}".format(ip,resolve(ip))
 def main():
-#    for ip in sys.stdin:
-#        ip = ip.strip()
-#        addr = reversename.from_address(ip)
-#        names = resolver.query(addr, "PTR")
-#        for name in names:
-#            print "{0} -> {1}".format(ip,name)
     r = reader_wrapper()
     w = writer_wrapper()
 
