56b2a73cc795a6087444ce2147256c7a89b7f811
alwabel <alwabel@usc.edu>
2016-08-18 15:52:35 -0700

    changes

--- resolve.py
+++ resolve.py
@@ -4,11 +4,14 @@ from dns import reversename
 import dns
 import sys
 import threading
-
+import time
+import bz2
 class reader_wrapper(object):
     def __init__(self,source=sys.stdin):
         self.lock = threading.Lock()
-        self.source = source
+        if type(source) == type(''):
+            self.source = open(source,"r")
+        else: self.source = source
     def next(self):
         with self.lock:
             return self.source.next()
@@ -18,10 +21,20 @@ class reader_wrapper(object):
 class writer_wrapper(object):
     def __init__(self,dest=sys.stdout):
         self.lock = threading.Lock()
-        self.dest = dest
+        if type(dest) == type(''):
+            if dest.endswith(".bz2"): self.dest = bz2.BZ2File(dest,"w")
+            else:self.dest = open(dest,"w")
+        else: self.dest = dest
     def write(self,msg):
         with self.lock:
             print >> self.dest, msg
+def int_to_ip(ip):
+    a,b,c,d = ip.split('.')
+    a = int(a)
+    b = int(b)
+    c = int(c)
+    d = int(d)
+    return (a<<24) | (b << 16) | (c<<8) | d
 
 def resolve(ip):
     addr = reversename.from_address(ip)
@@ -46,11 +59,16 @@ def resolve(ip):
 def process(r,w):
     for ip in r:
         ip = ip.strip()
-        w.write("{0}\t{1}".format(ip,','.join(resolve(ip))))
+        start = time.time()
+        #IPs = resolve(ip)
+        IPs = ['test']
+        end = time.time()
+        w.write("{0}\t{1}\t{2}\t{3}\t{4}".format(int_to_ip(ip),ip,','.join(IPs),end-start,start) )
         #print "{0} ---> {1}".format(ip,resolve(ip))
-def main():
-    r = reader_wrapper()
-    w = writer_wrapper()
+
+def start(input=sys.stdin,output=sys.stdout):
+    r = reader_wrapper(input)
+    w = writer_wrapper(output)
 
     MAX_THREADS = 10
     t = []
@@ -61,5 +79,8 @@ def main():
 
     for i in t:
         i.join()
+
+def main():
+    start()
 if __name__ == "__main__":
     main()
